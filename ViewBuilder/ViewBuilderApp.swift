6f8c77628eb51d78a6578bcce415630b9d0c96fd
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-08-25 18:54:09 +0300

    Initial Commit

---
+++ ViewBuilder/ViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderApp.swift
+//  ViewBuilder
+//
+//  Created by Sergey Lukaschuk on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
