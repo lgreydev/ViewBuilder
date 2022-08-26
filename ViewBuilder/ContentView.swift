af7346889308956a089869396950a6fe7b0ebf84
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-08-26 08:39:52 +0300

    Add custom view

--- ViewBuilder/ContentView.swift
+++ ViewBuilder/ContentView.swift
@@ -9,10 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                // Custom View
+            }
+            .navigationTitle("View Builder")
+        }
     }
 }
+
+struct AlertView<Content: View>: View {
+
+    let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        Text("Label")
+    }
+}
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
